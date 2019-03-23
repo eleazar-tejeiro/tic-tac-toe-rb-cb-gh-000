7b19cd7cdc1ea3155bc62a2f9acde9f201a83a3f
Uriel Eleazar Tejeiro Garcia <eleazartejeiro@gmail.com>
2019-03-23 03:42:13 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,16 +1,14 @@
-WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],
-  [0,4,8],[6,4,2] ]
 
-def play(board)
-  while !over?(board)
-    turn(board)
-  end
-  if won?(board)
-    puts "Congratulations #{winner(board)}!"
-  elsif draw?(board)
-    puts "Cat's Game!"
-  end
-end
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+]
 
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
@@ -20,67 +18,96 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def valid_move?(board, index)
-  index.between?(0,8) && !position_taken?(board, index)
-end
-
-def won?(board)
-  WIN_COMBINATIONS.detect do |combo|
-    board[combo[0]] == board[combo[1]] &&
-    board[combo[1]] == board[combo[2]] &&
-    position_taken?(board, combo[0])
-  end
-end
-
-def full?(board)
-  board.all?{|token| token == "X" || token == "O"}
-end
-
-def draw?(board)
-  !won?(board) && full?(board)
-end
-
-def over?(board)
-  won?(board) || draw?(board)
-end
-
 def input_to_index(user_input)
   user_input.to_i - 1
 end
 
+def move(board, index, current_player)
+  board[index] = current_player
+end
+
+def position_taken?(board, location)
+  board[location] != " " && board[location] != ""
+end
+
+def valid_move?(board, index)
+  index.between?(0,8) && !position_taken?(board, index)
+end
+
 def turn(board)
   puts "Please enter 1-9:"
-  user_input = gets.strip
-  index = input_to_index(user_input)
+  input = gets.strip
+  index = input_to_index(input)
   if valid_move?(board, index)
-    move(board, index, "#{current_player(board)}")
+    move(board, index,current_player(board))
     display_board(board)
   else
     turn(board)
   end
 end
 
-def position_taken?(board, index)
-  board[index]== "X" || board[index] == "O"
+def turn_count(board)
+  count=0
+  board.each do |pos|
+    if pos=="X" || pos=="O"
+      count+=1
+
+    end
+  end
+  return count
 end
 
 def current_player(board)
-  if turn_count(board) % 2 == 0
-    puts "X"
-  else
-    puts "O"
+  count = turn_count(board)
+  if count%2==0
+    return "X"
+  end
+  return "O"
 end
 
-def turn_count(board)
-  board.count{|token| token == "X" || token == "O"}
+def won?(board)
+  x_won = false
+  o_won = false
+  WIN_COMBINATIONS.each do |combi|
+    x_won = combi.all? { |e| board[e]=="X" }
+    o_won = combi.all? { |e| board[e]=="O" }
+    if x_won || o_won
+      return combi
+    end
+  end
+  return false
 end
 
-def move(board, index, player)
-  board[index] = player
+def full?(board)
+  return !(board.any? { |e| e==" "})
+end
+
+def draw?(board)
+  return full?(board) && !(won?(board))
+end
+
+def over?(board)
+  return draw?(board) || won?(board)
 end
 
 def winner(board)
-  if winning_combo = won?(board)
-    board[winning_combo.first]
+  result = won?(board)
+  if result==false
+    return nil
   end
+  return board[result[0]]
+end
+
+def play(board)
+  until over?(board) do
+    turn(board)
+  end
+
+  p_win = winner(board)
+  if p_win=="X" || p_win=="O"
+    puts "Congratulations #{p_win}!"
+  elsif draw?(board)
+    puts "Cat's Game!"
+  end
+
 end
